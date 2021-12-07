193b0c9a0006a67fadcc35450b63e65e312e1a23
Arun-Soundar <95679484+Arun-Soundar@users.noreply.github.com>
2021-12-07 17:54:20 +0530

    Add of three numbers

---
+++ sum_of_three_numbers.java
@@ -0,0 +1,14 @@
+package repojava;
+import java.util.Scanner;
+
+public class sum_of_three_numbers {
+	public static void main(String args[]) {
+	int a,b,c;
+	Scanner input = new Scanner(System.in);
+	a=input.nextInt(); 
+	b=input.nextInt(); 
+	c=input.nextInt();
+	System.out.println(a+b+c);
+
+	}
+}
